f8b095cce66b884361dec6d30df1f57f00baad42
hdubey64 <hdubey64@gmail.com>
2022-09-09 11:54:37 +0530

    MDN Math Test 01, 02, 03 Complete

--- fromMDNMathsTest03.js
+++ fromMDNMathsTest03.js
@@ -5,18 +5,39 @@ In the final task for this article, we want you to write some tests. You've got 
 // Statement 1:The elephant weighs less than the mouse
 var elephant = 1000;
 var mouse = 50;
-if (elephant > mouse) {
-   console.log(true);
+
+let weightComparison = elephant == mouse;
+const weightTest = weightComparison;
+
+if (weightTest == true) {
+   console.log("True — elephants weigh less than mice!?");
 } else {
-   console.log(false);
+   console.log("False — of course an elephant is heavier than a mouse!");
 }
 
 // Statement 2: The Ostrich is taller than the duck
 const ostrichHeight = 2;
 const duckHeight = 0.3;
 
-if (ostrichHeight > duckHeight) {
-   console.log(false);
+const heightComparison = ostrichHeight == duckHeight;
+const heightTest = heightComparison;
+
+if (heightTest == true) {
+   console.log("True — an ostrich is indeed taller than a duck!");
 } else {
-   console.log(true);
+   console.log("False — apparently a duck is taller than an ostrich!?");
+}
+
+// Statement 3: The two passwords match
+
+const pwd1 = "stromboli";
+const pwd2 = "stROmBoLi";
+
+const pwdMatch = pwd1 === pwd2;
+
+const pwdTest = pwdMatch;
+if (pwdTest == true) {
+   console.log("True — the passwords match.");
+} else {
+   console.log("False — the passwords do not match; please check them");
 }
