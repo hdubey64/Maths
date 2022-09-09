f6545b338c2a2becf0ea09d938f0d986293b346e
hdubey64 <hdubey64@gmail.com>
2022-09-09 13:18:05 +0530

    Handling text — strings in JavaScript

--- mathsTable.js
+++ mathsTable.js
@@ -1,6 +1,14 @@
 // Print Maths Table
 
-var table = 10;
-for (i = 1; i <= 10; i++) {
-   console.log(table * i);
+// var table = 10;
+// for (i = 1; i <= 10; i++) {
+//    console.log(table * i);
+// }
+
+const browserType = "mozilla";
+
+if (browserType.includes("zilla")) {
+   console.log("Found zilla!");
+} else {
+   console.log("No zilla here!");
 }
